c634461b824dd2153f7160a777d2610e88df57de
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-07-10 01:59:20 +0530

    update api url

--- client/api/doItBro.ts
+++ client/api/doItBro.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// let URL = "https://do-it-bro.herokuapp.com/api/";
-let URL =
-  "https://a952-2405-201-301b-3a9b-cb95-d3f9-27a3-a2c5.in.ngrok.io/api/";
+let URL = "https://do-it-bro.herokuapp.com/api/";
 
 export default axios.create({
   baseURL: URL,

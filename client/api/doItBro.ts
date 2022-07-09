654d61b7246210950b64159687a69a3cc826d52d
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-07-09 06:22:20 +0530

    change api route

--- client/api/doItBro.ts
+++ client/api/doItBro.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// let URL = "https://do-it-bro.herokuapp.com/api/";
-let URL =
-  "https://99c4-2405-201-301b-3a9b-6b98-e7c3-7529-6339.in.ngrok.io/api/";
+let URL = "https://do-it-bro.herokuapp.com/api/";
 
 export default axios.create({
   baseURL: URL,
